f35137263f1d16e739522d8bcf83fa533a8b4ab4
eduardovirtual <eduardosantoscosta2002@gmail.com>
2024-02-22 05:46:00 -0300

    Read me e seus assets adicionados, junto com comentários e reajustes no css e js

---
+++ README.md
@@ -0,0 +1,24 @@
+# Decodificador de Texto - Challenge ONE (Alura + Oracle)
+
+Nesse projeto, o desafio era desenvolver uma aplicação que criptografa textos, com o objetivo de trocar mensagens "secretas" com outras pessoas que saibam o segredo da criptografia utilizada.
+
+**As "chaves" de criptografia utilizadas são:**
+- A letra "e" é convertida para "enter"
+- A letra "i" é convertida para "imes"
+- A letra "a" é convertida para "ai"
+- A letra "o" é convertida para "ober"
+- A letra "u" é convertida para "ufat"
+
+**Exemplos:**
+- "gato" => "gaitober"
+- "gaitober" => "gato"
+
+A página contém campos de inserção de texto para então serem criptografados ou descriptografados, tudo que o usuário digitar, será exibido na tela!
+
+## Demonstração
+(./assets-md/demonstracao.png)
+
+## Responsividade
+
+Além disso, o projeto conta com uma responsividade para diferentes dispositivos!
+(./assets-md/responsividade.png)
--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// Função para ajustar dinamicamente a altura de um textarea com base no conteúdo
 function autoajuste(textarea) {
-    textarea.style.height = 'auto';
-    textarea.style.height = (textarea.scrollHeight) + 'px';
+    textarea.style.height = 'auto'; // Redefine a altura para automático
+    textarea.style.height = (textarea.scrollHeight) + 'px'; // Define a altura do textarea para a altura do seu conteúdo
 }
 
+// Função para criptografar texto substituindo determinadas letras por outras
 function criptografia(text) {
+    // Mapeia as substituições desejadas
     let substituicoes = {
         'e': 'enter',
         'i': 'imes',
@@ -11,6 +14,7 @@ function criptografia(text) {
         'o': 'ober',
         'u': 'ufat'
     };
+    // Realiza a substituição das letras no texto
     return text.split('').map(letra => {
         switch (letra) {
             case 'e':
@@ -29,7 +33,9 @@ function criptografia(text) {
     }).join('');
 }
 
+// Função para descriptografar texto revertendo as substituições feitas
 function descriptografia(text) {
+    // Mapeia as substituições inversas
     let substituicoes = {
         'enter': 'e',
         'imes': 'i',
@@ -40,6 +46,7 @@ function descriptografia(text) {
 
     let palavra = text;
 
+    // Realiza a substituição inversa das letras no texto
     for (let chave in substituicoes) {
         palavra = palavra.split(chave).join(substituicoes[chave]);
     }
@@ -47,37 +54,44 @@ function descriptografia(text) {
     return palavra;
 }
 
+// Função para exibir o texto de saída em um campo específico e controlar a visibilidade de elementos HTML
 function exibirSaida(text) {
     let campo = document.querySelector('.conteudoLateral__saida');
-    campo.value = text;
+    campo.value = text; // Define o valor do campo de saída como o texto fornecido
 
+    // Oculta um elemento HTML com a classe 'desaparecer'
     let divElement = document.querySelector('.desaparecer');
     divElement.style.visibility = 'hidden';
 
+    // Torna visível um elemento HTML com a classe 'conteudoLateral__btn3'
     divElement = document.querySelector('.conteudoLateral__btn3');
     divElement.style.visibility = 'visible';
 }
 
+// Função para limpar o conteúdo de um textarea
 function limparCampo() {
     let text = document.querySelector('textarea');
-    text.value = '';
+    text.value = ''; // Define o valor do textarea como vazio
 }
 
+// Função para receber o texto, criptografá-lo e exibir o resultado
 function receberCriptografar() {
-    let text = document.querySelector('textarea').value;
-    exibirSaida(criptografia(text));
-    limparCampo();
+    let text = document.querySelector('textarea').value; // Obtém o texto do textarea
+    exibirSaida(criptografia(text)); // Exibe o resultado da criptografia
+    limparCampo(); // Limpa o campo de entrada
 }
 
+// Função para receber o texto, descriptografá-lo e exibir o resultado
 function receberDescriptografia() {
-    let text = document.querySelector('textarea').value;
-    exibirSaida(descriptografia(text));
-    limparCampo();
+    let text = document.querySelector('textarea').value; // Obtém o texto do textarea
+    exibirSaida(descriptografia(text)); // Exibe o resultado da descriptografia
+    limparCampo(); // Limpa o campo de entrada
 }
 
+// Função para copiar o texto do campo de saída para a área de transferência
 function copiarText() {
     let copyText = document.getElementById("Copy");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
+    copyText.select(); // Seleciona o texto no campo de saída
+    copyText.setSelectionRange(0, 99999); // Define o intervalo de seleção como todo o texto
+    navigator.clipboard.writeText(copyText.value); // Copia o texto selecionado para a área de transferência
 }
